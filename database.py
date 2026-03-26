d923bee8610a90d58b0f301f7107039886969b32
gsd2003 <gautamdeshmukh2003@gmail.com>
2026-03-26 13:03:50 +0530

    "Database configured successfully"

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 
-db_url="mysql+pymysql://mariadb:root@db:3306/mydb"
+db_url="mysql+pymysql://root:root@172.31.44.234:3306/mydb"
 engine = create_engine(db_url,echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
